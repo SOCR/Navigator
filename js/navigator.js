3134be8eedd816bb9cbf991b8c5195c4f74dfd61
Patrick Tan <PatrickTioTan@gmail.com>
2013-05-30 12:41:19 -0700

    reformat navigator.js, new version of website

--- js/navigator.js
+++ js/navigator.js
@@ -1,20 +1,27 @@
 (function(){
+	$(document).ready(function(){
 
-var inputHandle = $("#xmlInput"); 
-var goButton = $("#goButton");
+		var inputHandle = $("#xmlInput"); 
+		var goButton = $("#goButton");
 
-goButton.on("click",function(){
-	var cleanData = clean(inputHandle.val());
-	var xmlObj = $.parseXML(cleanData);
-	console.log(xmlObj);
-});
+		goButton.on("click",function(){
+			var cleanData = clean(inputHandle.val());
+			var xmlObj = $.parseXML(cleanData);
+			console.log(xmlObj);
 
-function clean(data){
- 	data = data.replace(/(\r\n|\n|\r)/gm," ");
- 	data = data.replace(/\s+/g," ");
- 	data = data.replace(/^\s+|\s+$/g,'')
-	return data;
-}
+
+		});
+
+		function clean(data){
+		 	data = data.replace(/(\r\n|\n|\r)/gm," ");
+		 	data = data.replace(/\s+/g," ");
+		 	data = data.replace(/^\s+|\s+$/g,'')
+			return data;
+		}
+
+
+
+	}); //document ready
 })();
 
 
