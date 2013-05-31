7f6296871aee623f1992159ae090b49e4ca4907f
Patrick Tan <PatrickTioTan@gmail.com>
2013-05-30 20:18:57 -0700

    working version of converting xml to json and placing the data into arrays

--- js/navigator.js
+++ js/navigator.js
@@ -1,15 +1,24 @@
+
+
 (function(){
 	$(document).ready(function(){
 
 		var inputHandle = $("#xmlInput"); 
 		var goButton = $("#goButton");
 
+		var canvas=document.getElementById("viewport");
+		var ctx=canvas.getContext('2d');
+
 		goButton.on("click",function(){
+			var nodeName = "node";
+			var dataName = "name";
 			var cleanData = clean(inputHandle.val());
-			var xmlObj = $.parseXML(cleanData);
-			console.log(xmlObj);
-
-
+			var jsonObj = $.xml2json(cleanData);	
+			var mainNode;
+			mainNode = getData(jsonObj, nodeName, dataName);
+			console.log(mainNode);
+			document.getElementById("heirarchyOutput").innerHTML=JSON.stringify(mainNode, null, 4);
+			drawData(mainNode);
 		});
 
 		function clean(data){
@@ -19,9 +28,34 @@
 			return data;
 		}
 
+		function getData(jsonObj, nodeName, dataName){
+			var next = new Array();
+			var curNode = new dataNode(jsonObj.name, jsonObj.url, next);
 
+			if(jsonObj.node != null)
+			{
+				var size = jsonObj.node.length;
+			}
+			else{
+				return curNode;
+			}
+			for(var i = 0; i < size; i++)
+			{
+				curNode.array[i]=getData(jsonObj.node[i], nodeName, dataName);
+			}
+			return curNode;
+		}
 
+		function dataNode(name, url, array)
+		{
+			this.name = name;
+			this.url = url;
+			this.array = array;
+		}
+
+		function drawData(node)
+		{
+			
+		}
 	}); //document ready
 })();
-
-
